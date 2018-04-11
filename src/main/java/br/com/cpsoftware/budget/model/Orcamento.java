d76673f78fe9d60f1ccc1e58159d6fb8381bf150
PROJ2M04W\desenvolvedor <Michael>
2018-04-11 16:16:07 -0300

    Test

--- src/main/java/br/com/cpsoftware/budget/model/Orcamento.java
+++ src/main/java/br/com/cpsoftware/budget/model/Orcamento.java
@@ -1,16 +1,5 @@
-/**
- * 
- */
 package br.com.cpsoftware.budget.model;
 
-/**
- * @author desenvolvedor
- *
- */
 public class Orcamento {
-	private int orcamento_id;
-	private float valor;
-	private float valor_parcial;
-	
-	
+
 }
