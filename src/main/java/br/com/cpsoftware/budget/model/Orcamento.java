02fc6075174954942733b39cdf7f9e2cb6338f06
PROJ2M05W\desenvolvedor\michel <michel.lins17@gmail.com>
2018-04-11 16:31:04 -0300

    Implementação das classes Orcamento e Categoria do pacote model.

--- src/main/java/br/com/cpsoftware/budget/model/Orcamento.java
+++ src/main/java/br/com/cpsoftware/budget/model/Orcamento.java
@@ -1,5 +1,34 @@
 package br.com.cpsoftware.budget.model;
 
 public class Orcamento {
-
+	private String nome;
+	private float valor;
+	private float valor_parcial;
+	
+	
+	public Orcamento(String nome, float valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public float getValor_parcial() {
+		return valor_parcial;
+	}
+	public void setValor_parcial(float valor_parcial) {
+		this.valor_parcial = valor_parcial;
+	}
+	
+	
 }
