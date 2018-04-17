faf4e941bd1f54046c14c34051712948534a03bc
PROJ2M04W\desenvolvedor <Michael>
2018-04-17 11:37:52 -0300

    Implementação das classes dao.Item e model.Item.

--- src/main/java/br/com/cpsoftware/budget/dao/ItemDAO.java
+++ src/main/java/br/com/cpsoftware/budget/dao/ItemDAO.java
@@ -1,5 +1,71 @@
 package br.com.cpsoftware.budget.dao;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import br.com.cpsoftware.budget.model.Item;
+import br.com.cpsoftware.budget.model.Orcamento;
+
 public class ItemDAO{
- 
+	
+	/*
+	 * TODO Validação dos dados(entrada/banco)
+	 */
+	
+	private DatastoreService datastore;
+	private static final String ITEM_KIND = "Item";
+	
+	public ItemDAO() {
+		datastore = DatastoreServiceFactory.getDatastoreService();
+	}
+	
+	public Long create(Item item) {
+		Entity itemEntity = new Entity(ITEM_KIND);
+		itemEntity.setProperty(Item.ID, item.getId());
+		itemEntity.setProperty(Item.NOME, item.getNome());
+		itemEntity.setProperty(Item.DESCRICAO, item.getDescricao());
+		itemEntity.setProperty(Item.VALOR_UNIFORME, item.getValor_uniforme());
+		itemEntity.setProperty(Item.QUANTIDADE, item.getQuantidade());
+		
+		Key itemKey = datastore.put(itemEntity);
+		return itemKey.getId();
+	}
+
+	public Item read(Long itemId) {
+		try {
+		    Entity itemEntity = datastore.get(KeyFactory.createKey(ITEM_KIND, itemId));
+		    return entityToItem(itemEntity);
+		  } catch (EntityNotFoundException e) {
+		    return null;
+		  }
+	}
+
+	public void update(Item item) {
+		Key key = KeyFactory.createKey(ITEM_KIND, item.getId());  // From a book, create a Key
+		Entity itemEntity = new Entity(key);         // Convert Book to an Entity
+		itemEntity.setProperty(Orcamento.NOME, item.getNome());
+		itemEntity.setProperty(Item.DESCRICAO, item.getDescricao());
+		itemEntity.setProperty(Item.VALOR_UNIFORME, item.getValor_uniforme());
+		itemEntity.setProperty(Item.QUANTIDADE, item.getQuantidade());
+
+		datastore.put(itemEntity);                   // Update the Entity
+	}
+
+	public void delete(Long itemId) {
+		Key key = KeyFactory.createKey(ITEM_KIND, itemId);        // Create the Key
+		datastore.delete(key);                      // Delete the Entity
+	}
+	
+	private Item entityToItem(Entity itemEntity) {
+		return new Item((Long)itemEntity.getProperty(Item.ID),
+				 (String)itemEntity.getProperty(Item.NOME),
+				 (String)itemEntity.getProperty(Item.DESCRICAO),
+				 (Float)itemEntity.getProperty(Item.VALOR_UNIFORME),
+				 (int)itemEntity.getProperty(Item.QUANTIDADE));
+	}
+
 }
--- src/main/java/br/com/cpsoftware/budget/model/Item.java
+++ src/main/java/br/com/cpsoftware/budget/model/Item.java
@@ -1,19 +1,35 @@
 package br.com.cpsoftware.budget.model;
 
+
 public class Item {
+	private Long id;
 	private String nome;
 	private String descricao;
-	private int valor_uniforme;
+	private Float valor_uniforme;
 	private int quantidade;
 	
-	public Item(String nome, String descricao, int valor_uniforme, int quantidade) {
+	//Definição das keys do banco
+	public static final String ID = "id";
+	public static final String NOME = "nome";
+	public static final String DESCRICAO = "descricao";
+	public static final String VALOR_UNIFORME = "valor_uniforme";
+	public static final String QUANTIDADE = "quantidade";
+
+	public Item(Long id,String nome, String descricao, Float valor_uniforme, int quantidade) {
 		super();
+		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
 		this.valor_uniforme = valor_uniforme;
 		this.quantidade = quantidade;
 	}
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
@@ -26,10 +42,10 @@ public class Item {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	public int getValor_uniforme() {
+	public Float getValor_uniforme() {
 		return valor_uniforme;
 	}
-	public void setValor_uniforme(int valor_uniforme) {
+	public void setValor_uniforme(Float valor_uniforme) {
 		this.valor_uniforme = valor_uniforme;
 	}
 	public int getQuantidade() {
