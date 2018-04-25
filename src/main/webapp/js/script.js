b0cc93b2757bc00f1dbbbb887e75785af4301755
PROJ2M07W\PROJETOS <lucasgabrielpdoliveira@gmail.com>
2018-04-25 17:54:19 -0300

    Melhoramento da função javascript que da o resultado automatico da mutiplicação dos valores de itens pelo unitário

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,5 +1,5 @@
 function calcularValorTotalItem() {
-	  var n1 = parseInt(document.getElementById('quantidadeItem').value);
-	  var n2 = parseInt(document.getElementById('valorUniformeItem').value);
-	  document.getElementById('resultado').value = "R$ "+ n1 * n2;
-	}
+	var quantidadeItens = parseFloat(document.getElementById('quantidadeItem').value);
+	var valorItens = parseFloat(document.getElementById('valorUniformeItem').value);
+	document.getElementById('resultado').value = "R$ "+ quantidadeItens * valorItens;
+}
