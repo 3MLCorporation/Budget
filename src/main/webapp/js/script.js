93c41872fdd838811d83bbff6984d19e07956009
Michel <michel.lins17@gmail.com>
2018-05-16 14:09:47 -0300

    Correção de erro no script.js;

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -19,16 +19,3 @@ function getStatusOrcamento(orcamentoSelectCount){
 	var statusSelecionado = select.options[select.selectedIndex].value;
 	document.getElementById('statusOrcamento' + orcamentoSelectCount).value = statusSelecionado;
 }
-
-$(document).ready(function(){
-	$("#myInput").on("keyup", function() {
-		var value = $(this).val().toLowerCase();
-		$("#myTable tr").filter(function() {
-			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-		});
-	});
-});
-
-.botao{
-	float:right
-}
