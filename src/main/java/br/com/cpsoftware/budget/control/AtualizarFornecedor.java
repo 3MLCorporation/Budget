e5c74f4fd9ceebe37439bddd0e1410ce7df6cc3c
Michel <>
2018-09-03 09:24:36 -0300

    Implementação do atualizarFornecedor;

--- src/main/java/br/com/cpsoftware/budget/control/AtualizarFornecedor.java
+++ src/main/java/br/com/cpsoftware/budget/control/AtualizarFornecedor.java
@@ -7,13 +7,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.cpsoftware.budget.dao.FornecedorDAO;
+import br.com.cpsoftware.budget.dao.UnidadeFederativaDAO;
+import br.com.cpsoftware.budget.model.Fornecedor;
+import br.com.cpsoftware.budget.model.Usuario;
+
 @SuppressWarnings("serial")
 public class AtualizarFornecedor extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
-        req.setAttribute("page", "atualizarFornecedor");           
+    	Fornecedor fornecedor = new FornecedorDAO().read(Long.parseLong(req.getParameter("fornecedorId")));
+    	req.setAttribute("fornecedor", fornecedor);
+    	req.setAttribute("ufs", new UnidadeFederativaDAO().getUnidadesFederativas());
+        req.setAttribute("page", "atualizarFornecedor");          
         req.getRequestDispatcher("/WEB-INF/base.jsp").forward(req, resp);
         
+    }
+    
+    @Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+    	Long fornecedorId = Long.parseLong(req.getParameter("fornecedorId"));
+		String nomeFantasia = req.getParameter("nomeFantasia");
+		String razaoSocial = req.getParameter("razaoSocial");
+		String cnpj = req.getParameter("cnpj");
+		String uf = req.getParameter("uf");
+		
+		FornecedorDAO fornecedorDAO = new FornecedorDAO();
+		Fornecedor fornecedor = fornecedorDAO.read(fornecedorId);
+
+		fornecedor.setNomeFantasia(nomeFantasia);
+		fornecedor.setRazaoSocial(razaoSocial);
+		fornecedor.setCnpj(cnpj);
+		fornecedor.setUf(uf);
+		
+		fornecedorDAO.update(fornecedor);
+
+		resp.sendRedirect("/principal");
     }
 }
