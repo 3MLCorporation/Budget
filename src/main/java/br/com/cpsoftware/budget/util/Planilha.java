b320c38404125f8eb578ce22f3717eb497176684
michelthomas <michel.lins17@gmail.com>
2018-09-25 03:00:26 -0300

    Continuação da implementação da função de gerar planilha;

--- src/main/java/br/com/cpsoftware/budget/util/Planilha.java
+++ src/main/java/br/com/cpsoftware/budget/util/Planilha.java
@@ -2,13 +2,17 @@ package br.com.cpsoftware.budget.util;
 
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletOutputStream;
 
+import org.apache.poi.ss.usermodel.BuiltinFormats;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
@@ -44,18 +48,25 @@ public class Planilha {
 	private static NotaFiscalDAO notaFiscalDAO = new NotaFiscalDAO();
 	private static PagamentoDAO pagamentoDAO = new PagamentoDAO();
 
-	private static int JANEIRO_COLUNA = 1;
-	private static int FEVEREIRO_COLUNA = 2;
-	private static int MARÇO_COLUNA = 3;
-	private static int ABRIL_COLUNA = 4;
-	private static int MAIO_COLUNA = 5;
-	private static int JUNHO_COLUNA = 6;
-	private static int JULHO_COLUNA = 7;
-	private static int AGOSTO_COLUNA = 8;
-	private static int SETEMBRO_COLUNA = 9;
-	private static int OUTUBRO_COLUNA = 10;
-	private static int NOVEMBRO_COLUNA = 11;
-	private static int DEZEMBRO_COLUNA = 12;
+	private final static int JANEIRO_COLUNA = 1;
+	private final static int FEVEREIRO_COLUNA = 2;
+	private final static int MARÇO_COLUNA = 3;
+	private final static int ABRIL_COLUNA = 4;
+	private final static int MAIO_COLUNA = 5;
+	private final static int JUNHO_COLUNA = 6;
+	private final static int JULHO_COLUNA = 7;
+	private final static int AGOSTO_COLUNA = 8;
+	private final static int SETEMBRO_COLUNA = 9;
+	private final static int OUTUBRO_COLUNA = 10;
+	private final static int NOVEMBRO_COLUNA = 11;
+	private final static int DEZEMBRO_COLUNA = 12;
+
+	private final static String HEADER_STYLE_CATEGORIA = "categoriaHeader";
+	private final static String HEADER_STYLE_CATEGORIA_NUMERO = "categoriaNumeroHeader";
+	private final static String HEADER_STYLE_CATEGORIA_VALOR= "categoriaValorHeader";
+	private final static String HEADER_STYLE_RUBRICA = "rubricaHeader";
+	private final static String HEADER_STYLE_RUBRICA_VALOR = "rubricaValorHeader";
+	private final static String HEADER_STYLE_ITEM = "itemHeader";
 	
 	public static void gerarPlanilha(Long projetoId, ServletOutputStream outputStream) throws IOException {
 		
@@ -109,6 +120,11 @@ public class Planilha {
         for(int i = 1; i < 5; i++) {
         	sheet.createRow(i).createCell(4).setCellValue(headerColumn[i - 1]);
         }
+
+        sheet.setColumnWidth(0, 256*2);
+        sheet.setColumnWidth(1, 256*8);
+        sheet.setColumnWidth(2, 256*2);
+        sheet.setColumnWidth(3, 256*40);
         
         Row headerRow = sheet.createRow(5);
         for(int i = 0; i < monthHeadersColumns.length; i++) {
@@ -125,8 +141,10 @@ public class Planilha {
         
         sheet.createFreezePane(0, 6);
         
+        Map<String, CellStyle> styles = createStyles(workbook);
+        
         for(Orcamento orcamento : orcamentos) {
-			addOrcamento(sheet, orcamento);
+			addOrcamento(sheet, styles, orcamento);
 		}
 
         /*// Create Cell Style for formatting Date
@@ -162,75 +180,105 @@ public class Planilha {
         outputStream.close();
 	}
 
-	private static void addOrcamento(Sheet sheet, Orcamento orcamento) {
-		
-		Row orcamentoRow = sheet.createRow(sheet.getLastRowNum() + 1);
-		orcamentoRow.createCell(0).setCellValue(orcamento.getNome());
+	private static void addOrcamento(Sheet sheet, Map<String, CellStyle> styles, Orcamento orcamento) {
 		//sheet.autoSizeColumn(0);
 		for(Categoria categoria : categoriaDAO.getCategorias(orcamento.getId())) {
-			Row categoriaRow = sheet.createRow(sheet.getLastRowNum() + 1);
-			categoriaRow.createCell(0).setCellValue(categoria.getNome());
-			for(Rubrica rubrica : rubricaDAO.getRubricas(categoria.getId())) {
-				Row rubricaRow = sheet.createRow(sheet.getLastRowNum() + 1);
-				rubricaRow.createCell(0).setCellValue(rubrica.getNome());
-				for(Item item : itemDAO.getItens(rubrica.getId())) {
-					Row itemRow = sheet.createRow(sheet.getLastRowNum() + 1);
-					itemRow.createCell(0).setCellValue(item.getNome());
-
-					Calendar cal = Calendar.getInstance();
-					
-					for(NotaFiscal nota :notaFiscalDAO.getNotasFiscais(item.getId())) {
-						cal.setTime(nota.getData());
-						int mes = cal.get(Calendar.MONTH);
-						setMonthCellValue(itemRow, mes + 1, nota.getValor());
-						/*
-						 * Tanto código e tempo pra no final dar 10 linhas
-						 * 
-						 * switch(cal.get(Calendar.MONTH)) {
-						case Calendar.JANUARY:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.FEBRUARY:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.MARCH:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.APRIL:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.MAY:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.JUNE:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.JULY:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.AUGUST:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.SEPTEMBER:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.OCTOBER:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.NOVEMBER:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						case Calendar.DECEMBER:
-							setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
-							break;
-						}*/
-					}
-				}
-			}
+			addCategoria(sheet, styles, categoria);
 		}
-		
 	}
 	
+
+	private static void addCategoria(Sheet sheet, Map<String, CellStyle> styles, Categoria categoria) {
+		Row categoriaRow = sheet.createRow(sheet.getLastRowNum() + 1);
+		Cell categoriaCell = categoriaRow.createCell(2);
+		categoriaCell.setCellValue(categoria.getNome().toUpperCase());
+		categoriaCell.setCellStyle(styles.get(HEADER_STYLE_CATEGORIA));
+		String formula = "";
+		for(Rubrica rubrica : rubricaDAO.getRubricas(categoria.getId())) {
+			formula = addRubrica(sheet, styles, rubrica);
+		}
+		for(int i = 4; i < 20; i++) {
+			categoriaRow.createCell(i).setCellStyle(styles.get(HEADER_STYLE_CATEGORIA_VALOR));
+		}
+	}
+	
+	/*private static CellStyle getStyle(Workbook workbook, int styleType) {
+		switch(styleType) {
+		case HEADER_STYLE_CATEGORIA:
+			CellStyle cellStyle = workbook.createCellStyle();
+			cellStyle.setFont(headerFont);
+			cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			break;
+		}
+	}*/
+
+	private static String addRubrica(Sheet sheet, Map<String, CellStyle> styles, Rubrica rubrica) {
+		Row rubricaRow = sheet.createRow(sheet.getLastRowNum() + 1);
+		Cell rubricaCell = rubricaRow.createCell(2);
+		rubricaCell.setCellValue(rubrica.getNome());
+		rubricaCell.setCellStyle(styles.get(HEADER_STYLE_CATEGORIA));
+		for(Item item : itemDAO.getItens(rubrica.getId())) {
+			addItem(sheet, styles, item);
+		}
+		for(int i = 4; i < 20; i++) {
+			rubricaRow.createCell(i).setCellStyle(styles.get(HEADER_STYLE_RUBRICA_VALOR));
+		}
+		return "dummy_data";
+	}
+
+	private static void addItem(Sheet sheet, Map<String, CellStyle> styles, Item item) {
+		Row itemRow = sheet.createRow(sheet.getLastRowNum() + 1);
+		itemRow.createCell(3).setCellValue(item.getNome());
+
+		Calendar cal = Calendar.getInstance();
+		
+		for(NotaFiscal nota :notaFiscalDAO.getNotasFiscais(item.getId())) {
+			cal.setTime(nota.getData());
+			int mes = cal.get(Calendar.MONTH);
+			setMonthCellValue(itemRow, mes + 4, nota.getValor());
+			/*
+			 * Tanto código e tempo pra no final dar 10 linhas
+			 * 
+			 * switch(cal.get(Calendar.MONTH)) {
+			case Calendar.JANUARY:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.FEBRUARY:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.MARCH:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.APRIL:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.MAY:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.JUNE:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.JULY:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.AUGUST:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.SEPTEMBER:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.OCTOBER:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.NOVEMBER:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			case Calendar.DECEMBER:
+				setMonthCellValue(itemRow, JANEIRO_COLUNA, nota.getValor());
+				break;
+			}*/
+		}
+	}
 
 	private static void setMonthCellValue(Row itemRow, int mes, Double valor){
 		if(itemRow.getCell(mes) == null) {
@@ -240,5 +288,37 @@ public class Planilha {
 			itemRow.getCell(mes).setCellValue(valorAnterior + valor);
 		}
 	}
-
+	
+	private static Map<String, CellStyle> createStyles(Workbook wb){
+		Map<String, CellStyle> styles = new HashMap<>();
+		DataFormat df = wb.createDataFormat();
+		
+		CellStyle style = wb.createCellStyle();
+		Font headerFont = wb.createFont();
+        headerFont.setBold(true);
+		style.setFont(headerFont);
+        style.setAlignment(HorizontalAlignment.LEFT);
+        styles.put(HEADER_STYLE_CATEGORIA_NUMERO, style);
+        styles.put(HEADER_STYLE_CATEGORIA, style);
+        
+        style = wb.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        Font fontValor = wb.createFont();
+        fontValor.setColor(IndexedColors.BLUE.getIndex());
+        fontValor.setBold(true);
+        style.setFont(fontValor);
+        style.setDataFormat(df.getFormat("R$ #,#0.00"));
+        styles.put(HEADER_STYLE_CATEGORIA_VALOR, style);
+        
+        style = wb.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        Font fontRubricaValor = wb.createFont();
+        fontRubricaValor.setColor(IndexedColors.BLACK.getIndex());
+        fontRubricaValor.setBold(true);
+        style.setFont(fontValor);
+        style.setDataFormat(df.getFormat("R$ #,#0.00"));
+        styles.put(HEADER_STYLE_RUBRICA_VALOR, style);
+        
+        return styles;
+	}
 }
